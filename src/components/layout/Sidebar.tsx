cd5ab8e5f46a6023bd3941150eff8e48fb31dbf3
MarcoBerumen <89665061+MarcoBerumen@users.noreply.github.com>
2022-08-09 19:29:52 -0500

    sidebar progress

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,4 +1,5 @@
 import 'boxicons';
+const logo = require('../../assets/img/logo.webp');
 
 export const Sidebar = () => {
   return (
@@ -6,7 +7,7 @@ export const Sidebar = () => {
       <header>
         <div className='image-text'>
           <span className='image'>
-            <img src='logo' alt='logo' />
+            <img src={logo} alt='logo' />
           </span>
           <div className='text header-text'>
             <span className='name'>Marco Berumen</span>
@@ -15,7 +16,74 @@ export const Sidebar = () => {
         </div>
         <i className='bx bx-chevron-right toggle'></i>
       </header>
-      <div className='menu-bar'></div>
+      <div className='menu-bar'>
+        <div className='manu-bar'>
+          <li className='search-box'>
+            <a href='#'>
+              <i className='bx bx-search icon'></i>
+              <input type='search' placeholder='Search...' />
+            </a>
+          </li>
+          <ul className='menu-links'>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-home-alt icon'></i>
+                <span className='text nav-text'> Dashboard</span>
+              </a>
+            </li>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-bar-chart-alt-2 icon'></i>
+                <span className='text nav-text'> Revenue</span>
+              </a>
+            </li>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-bell icon'></i>
+                <span className='text nav-text'> Notifications</span>
+              </a>
+            </li>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-pie-chart-alt icon'></i>
+                <span className='text nav-text'> Analytics</span>
+              </a>
+            </li>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-heart icon'></i>
+                <span className='text nav-text'> Likes</span>
+              </a>
+            </li>
+            <li className='nav-link'>
+              <a href='#'>
+                <i className='bx bx-wallet icon'></i>
+                <span className='text nav-text'> Wallets</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='bottom-content'>
+          <li className=''>
+            <a href='#'>
+              <i className='bx bx-log-out icon'></i>
+              <span className='text nav-text'> Logout</span>
+            </a>
+          </li>
+          <li className='mode'>
+            <div className='moon-sun'>
+              <i className='bx bx-moon icon moon'></i>
+              <i className='bx bx-sun icon sun'></i>
+            </div>
+            <span className='mode-text text'>Dark Mode</span>
+
+            <div className='toggle-switch'>
+              <span className='switch'></span>
+            </div>
+          </li>
+        </div>
+      </div>
     </nav>
   );
 };
